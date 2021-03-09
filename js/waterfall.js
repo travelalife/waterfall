b49c73f112f003fa0b65d46394bd771dc7cc7e6b
Liu <1062591116@qq.com>
2021-03-09 21:56:54 +0800

    添加滚动加载

--- js/waterfall.js
+++ js/waterfall.js
@@ -1,10 +1,4 @@
 (function (){
-    let _defaultOpts = {
-        width: 240,
-        marginX: 20,
-        marginY: 20
-    }
-
     let utils = {
         extend: function (target) {
             for (let i = 1, len = arguments.length; i < len; i++) {
@@ -20,28 +14,52 @@
     }
 
     function Waterfall(opts) {
-        init(opts);
+        this.init(opts);
     }
 
-    function init(s) {
-        const imgs = s.imgs || [];
+    Waterfall._defaultOpts = {
+        width: 240,
+        marginX: 20,
+        marginY: 20
+    }
 
-        let opts = utils.extend({}, _defaultOpts, s);
-        let $main = document.querySelector(opts.container);  // 外层容器
-        let width = parseFloat(getComputedStyle($main, null).width.replace("px", ""));  // 外层容器宽度
+    let proto = Waterfall.prototype;
 
-        const colWidth = opts.width + opts.marginX;
-        const colLength = Math.floor((width - opts.marginX) / (opts.width + opts.marginX));  // 总列数
+    let loading = false;
 
-        let containerWidth = colLength * opts.width + (colLength - 1) * opts.marginX;  // 容器宽度
-        $main.innerHTML = '<div class="waterfall-container" style="width: ' + containerWidth + 'px;position: relative;min-height: 1px;margin: 0 auto;"></div>';
-        let $container = $main.childNodes[0]; // 瀑布流存放容器
+    proto.init = function (s) {
+        this.opts = utils.extend({}, this.constructor._defaultOpts, s);
+        this.wrapper = document.querySelector(this.opts.container);  // 外层容器
+        let width = parseFloat(getComputedStyle(this.wrapper, null).width.replace("px", ""));  // 外层容器宽度
 
-        let colHeightArr = []; // 每列的高度
-        for (let i = 0; i < colLength; i++) {
-            colHeightArr.push(0);
+        this.colWidth = this.opts.width + this.opts.marginX;
+        this.colLength = Math.floor((width - this.opts.marginX) / (this.opts.width + this.opts.marginX));  // 总列数
+
+        this.containerWidth = this.colLength * this.opts.width + (this.colLength - 1) * this.opts.marginX;  // 容器宽度
+        this.wrapper.innerHTML = '<div class="waterfall-container" style="width: ' + this.containerWidth + 'px;position: relative;min-height: 1px;margin: 0 auto;"></div>';
+        this._container = this.wrapper.childNodes[0]; // 瀑布流存放容器
+
+        this.colHeightArr = []; // 每列的高度
+        for (let i = 0; i < this.colLength; i++) {
+            this.colHeightArr.push(0);
         }
 
+        this.loadMore(this.opts.imgs);
+
+        const self = this;
+        this.wrapper.addEventListener('scroll', function (e) {
+            const max = Math.max.apply(null, self.colHeightArr);
+            if (this.scrollTop + this.offsetHeight >= max - 10) {
+                if (loading) {
+                    return false;
+                }
+                loading = true;
+                self.loadMore(getData());
+            }
+        })
+    }
+
+    proto.loadMore = function (imgs) {
         // 计算并记录所有图片的高度
         let imgArr = [];
         let promiseArr = [];
@@ -50,12 +68,12 @@
             const url = imgEle.src;
             let img = new Image();
             img.src = url;
-            imgArr.push({
+            imgArr.push(utils.extend({}, {
                 ins: img,
                 src: url,
                 title: imgEle.title,
                 desc: imgEle.desc
-            });
+            }));
             const p = img.decode().then(() => {
 
             }).catch(() => {
@@ -69,16 +87,16 @@
             for (let i = 0; i < imgArr.length; i++) {
                 const imgObj = imgArr[i];
                 const imgIns = imgObj.ins;
-                const height = opts.width / imgIns.width * imgIns.height;
-                const minCol = Math.min(...colHeightArr); // 最小高度
-                const minColIndex = colHeightArr.indexOf(minCol); // 最小高度的index
+                const height = this.opts.width / imgIns.width * imgIns.height;
+                const minCol = Math.min(...this.colHeightArr); // 最小高度
+                const minColIndex = this.colHeightArr.indexOf(minCol); // 最小高度的index
                 // 设置left和top值
-                imgObj.top = minCol + opts.marginY;
-                imgObj.left = minColIndex * colWidth;
+                imgObj.top = minCol + this.opts.marginY;
+                imgObj.left = minColIndex * this.colWidth;
 
-                colHeightArr[minColIndex] += height + opts.marginY;
+                this.colHeightArr[minColIndex] += height + this.opts.marginY;
 
-                imgHtml += '<div class="waterfall-item" style="width: ' + opts.width + 'px;left: ' + imgObj.left + 'px;top: ' + imgObj.top + 'px;position: absolute;box-shadow: 0 0 4px #CCC;">' +
+                imgHtml += '<div class="waterfall-item" style="width: ' + this.opts.width + 'px;left: ' + imgObj.left + 'px;top: ' + imgObj.top + 'px;position: absolute;box-shadow: 0 0 4px #CCC;">' +
                     '<img src="' + imgObj.src + '" class="waterfall-img" style="display: block;width: 100%;" alt="' + imgObj.title + '">' +
                     '<div class="waterfall-desc" style="position: absolute;left: 0;bottom: 0;">' + imgObj.desc + '</div></div>';
             }
@@ -91,7 +109,9 @@
                 const child = children[i];
                 fragment.appendChild(child);
             }
-            $container.appendChild(fragment);
+            this._container.appendChild(fragment);
+        }).finally(() => {
+            loading = false;
         })
     }
 
